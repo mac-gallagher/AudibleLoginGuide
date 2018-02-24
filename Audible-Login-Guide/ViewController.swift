c15529667d8fd7b0293fccc3ba677eb600576e89
mac-gallagher <jmgallagher36@gmail.com>
2018-02-24 14:32:52 -0600

    Initial Commit

---
+++ Audible-Login-Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Audible-Login-Guide
+//
+//  Created by Mac Gallagher on 2/24/18.
+//  Copyright © 2018 Mac Gallagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
